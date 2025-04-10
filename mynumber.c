a7f83f95b3642b966445e85c431e00961bb88196
alswo05 <gmj835@naver.com>
2025-04-11 04:14:09 +0900

    fix mynumber.c

--- mynumber.c
+++ mynumber.c
@@ -2,11 +2,13 @@
 
 int main() {
 	for (int i = 1; i <= 100; i++) {
-		if (i % 2 != 0) printf(i, " ");
+		if (i % 2 != 0) printf("%d ", i);
+	}
 	printf("\n");
 	
 	for (int i = 1; i <= 100; i++) {
-		if (i % 2 == 0) printf(i, " ");
+		if (i % 2 == 0) printf("%d ", i);
+	}
 	printf("\n");
 	
 	return 0;
