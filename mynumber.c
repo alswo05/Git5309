4c222f2191fda7629b91ec40314342714983731b
alswo05 <gmj835@naver.com>
2025-04-11 03:30:05 +0900

    modify mynumber.c

--- mynumber.c
+++ mynumber.c
@@ -3,4 +3,11 @@
 int main() {
 	for (int i = 1; i <= 100; i++) {
 		if (i % 2 != 0) printf(i, " ");
+	printf("\n");
+	
+	for (int i = 1; i <= 100; i++) {
+		if (i % 2 == 0) printf(i, " ");
+	printf("\n");
+	
+	return 0;
 }
